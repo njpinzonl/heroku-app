d4a960ab49cf4f0151f403adba4bca3bafb64fcf
Nelson Javier Pinzon Lopez <njpilo@gmail.com>
2018-09-26 08:42:11 -0500

    new projects

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directorys
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
